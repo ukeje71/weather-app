1f073d420ca3e7ac803c4e232cbb3a9c36b05778
ukeje isaac <ukejeisaac71@gmail.com>
2025-04-02 14:10:57 +0630

    feature:Changed from  css styles to Tailwind styles

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
 let valueSearch =document.getElementById("valueSearch");
 let city =document.getElementById("city");
-let temperature =document.getElementById("temperature");
-let description =document.querySelector("description");
+let temperature = document.getElementById("temperature");
+let description =document.querySelector(".description");
 let cloudy =document.getElementById("cloudy");
 let humidity =document.getElementById("humidity");
 let pressure =document.getElementById("pressure");
 let form = document.querySelector("form");
-form.addEventListener('submit',(submit)=>{
-    e.preventDefault();
-    if (valueSearch.value != ""){
-        getApi;
+form.addEventListener('submit',(event) => {
+    event.preventDefault();
+    if (valueSearch.value != ''){
+        searchWeather();
     }
 })
 
-let id='9505fd1df737e20152fbd78cdb289b6a';
-let url='https://api.openweather.map.org/data/2.5/weather?units=metric&appid='+id;
-const  getApi = () =>{
- fetch(url + '&q='+valueSearch.value)
- .then(Responseive =>Responseive.json())
+let id='9caba1bb58bdfca7cc24fd8fea34e1ef';
+let url=' https://api.openweathermap.org/data/2.5/weather?q=${city}&appid='+ id;
+const  searchWeather = () =>{ 
+ fetch(url + '&q=' + valueSearch.value)
+ .then(responsive =>responsive.json())
  .then(data => {
     console.log(data);
-    if(data.cod == 200){
-        city.querySelector('figcaption').innerText =data.name;
-        img.querySelector("img").src='https//flagsapi.com/'+data.sys.country+'/shiny/32.png'
-
-    }
  })
 }
