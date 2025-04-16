cd26efa870d1c1a99947e7f53428205cd0ff144f
ukeje isaac <ukejeisaac71@gmail.com>
2025-04-16 11:47:15 +0630

    debug :Completed API processes

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
-let valueSearch =document.getElementById("valueSearch");
-let city =document.getElementById("city");
+let valueSearch = document.getElementById("valueSearch");
+let city = document.getElementById("city");
 let temperature = document.getElementById("temperature");
-let description =document.querySelector(".description");
-let cloudy =document.getElementById("cloudy");
-let humidity =document.getElementById("humidity");
-let pressure =document.getElementById("pressure");
+let description = document.querySelector(".description");
+let cloudy = document.getElementById("cloudy");
+let humidity = document.getElementById("humidity");
+let Pressure = document.getElementById("Pressure");
 let form = document.querySelector("form");
-let flag=document.getElementById("flag");
-let weather =document.getElementById("weather");
-form.addEventListener('submit',(event) => {
-    event.preventDefault();
-    if (valueSearch.value != ''){
-        searchWeather();
-    }
-})
+let flag = document.getElementById("flag");
+let weather = document.getElementById("weather");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (valueSearch.value != "") {
+    searchWeather();
+  }
+});
 
-let id='9b89417f1cde6a57c878c774a4fe2746';
-let url='https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + id;
-const  searchWeather = () =>{ 
- fetch(url + '&q=' + valueSearch.value)
- .then(responsive =>responsive.json())
- .then(data => {
-    console.log(data);
+let id = "9b89417f1cde6a57c878c774a4fe2746";
+let url ="https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" + id;
+const searchWeather = () => {
+  fetch(url + "&q=" + valueSearch.value)
+    .then((responsive) => responsive.json())
+    .then((data) => {
+      console.log(data);
 
-    if(data.cod ==200){
+      if (data.cod == 200) {
         city.innerText = data.name;
-        flag.src ='https://flagsapi.com/'+data.sys.country+'/shiny/32.png';
-        weather.src ='http://openweathermap.org/img/w/'+data.weather[0].icon+'.png'
-        temperature.textContent=data.main.temp;
-         
-    }
- })
-}
+        flag.src = "https://flagsapi.com/" + data.sys.country + "/shiny/32.png";
+        weather.src =
+          "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+        temperature.textContent = data.main.temp;
+        cloudy.textContent = data.clouds.all;
+        humidity.innerText = data.main.humidity;
+        Pressure.innerText = data.main.pressure;
+      }
+      else{
+        main.classList.add('error');
+      }
+    });
+};
